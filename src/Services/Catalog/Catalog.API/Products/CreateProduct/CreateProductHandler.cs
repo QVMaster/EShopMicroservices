70aa4df39d54ba824f9ec2cd8268f265834cea9d
Ali <alimh76@gmail.com>
2024-07-31 22:43:35 +0330

    05 - Catalog

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(Guid Id, string Name, List<string> category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult> ;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a produjct
+
+        throw new NotImplementedException();
+    }
+}
