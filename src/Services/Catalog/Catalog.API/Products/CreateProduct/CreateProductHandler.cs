d6c58c714aa8649ec892ac2a76f41bb25269bd3d
Ali <alimh76@gmail.com>
2024-08-05 19:51:09 +0330

    06-10 Catalog - postgres container in docker compose

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
+﻿
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult> ;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler(IDocumentSession session) 
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -25,9 +23,10 @@ internal class CreateProductCommandHandler
         };
 
         // Save it to db
-
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // return result -> CreateProductResult
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,9 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 
 builder.Services.AddCarter();
-
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    //opt.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.CreateOrUpdate; // for development phase
+    //opt.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.None; // for production phase
 });
 
 
